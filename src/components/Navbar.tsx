6d88a8973ae7c1c70abb4147c8344dd7cf274b1d
Jude Odey <ohotujude@gmail.com>
2025-10-30 12:43:27 +0100

    made changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Menu, X, ChevronDown, LogOut, User, Home, ShoppingBag, Info, Phone } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
