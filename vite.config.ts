493725bc4ee54c4ba3eda4ae182bcb6bb290e2a9
Jude Odey <ohotujude@gmail.com>
2026-03-25 16:49:23 +0100

    fix(config): use dynamic env config for vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,45 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  // theme: {
-  //   extend: {
-  //     fontFamily: {
-  //       nunito: ['"Nunito Sans"', 'sans-serif'],
-  //       jakarta: ['"Plus Jakarta Sans"', 'sans-serif'],
-  //       dm: ['"DM Sans"', 'sans-serif'],
-  //     },
-  //   },
-  // },
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    // theme: {
+    //   extend: {
+    //     fontFamily: {
+    //       nunito: ['"Nunito Sans"', 'sans-serif'],
+    //       jakarta: ['"Plus Jakarta Sans"', 'sans-serif'],
+    //       dm: ['"DM Sans"', 'sans-serif'],
+    //     },
+    //   },
+    // },
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'recharts': ['recharts'],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'recharts': ['recharts'],
+          },
         },
       },
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.farmchops.com',
-        changeOrigin: true,
-        secure: false,
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL ? env.VITE_API_BASE_URL.replace(/\/api$/, '') : '',
+          changeOrigin: true,
+          secure: false,
+        }
       }
     }
   }
